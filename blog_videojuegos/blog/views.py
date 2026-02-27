c8568ff9734d28ae37cf8d5ae8cb47ade880a645
Gabriel Gonzalez <gabrielgonzalezbotto@gmail.com>
2026-02-27 15:45:32 -0300

    creación de funcion post_juego

--- blog_videojuegos/blog/views.py
+++ blog_videojuegos/blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+def post_juego(request):
+    juegos = [
+        {'nombre': 'Battletoads', 'plataforma':'SEGA genesis'},
+        {'nombre': 'Boogerman', 'plataforma': 'SEGA genesis'},
+        {'nombre': 'Cadillacs & Dinosaurs', 'plataforma': 'ARCADE'},
+        {'nombre': 'Bloody Roar', 'plataforma': 'PlayStation'}
+    ]
+
+    contexto_post_juego = {'post_juego': juegos}
+    return render(request, 'blog/post_juego.html', contexto_post_juego)
