67249c825912b5966fd1fd3072d8d25924b1196f
Gabriel Gonzalez <gabrielgonzalezbotto@gmail.com>
2026-03-03 15:19:44 -0300

    arreglo en la carga de datos en la plantilla Blog

--- blog_videojuegos/blog/views.py
+++ blog_videojuegos/blog/views.py
@@ -3,11 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 def post_juego(request):
     juegos = [
-        {'nombre': 'Battletoads', 'plataforma':'SEGA genesis'},
-        {'nombre': 'Boogerman', 'plataforma': 'SEGA genesis'},
-        {'nombre': 'Cadillacs & Dinosaurs', 'plataforma': 'ARCADE'},
-        {'nombre': 'Bloody Roar', 'plataforma': 'PlayStation'}
+        {'nombre_usuario': 'Gabriel Gonzalez Botto', 'fecha': '22/02/26', 'nombre': 'Battletoads', 'plataforma':'SEGA genesis'},
+        {'nombre_usuario': 'Martina Gonzalez', 'fecha': '15/02/26', 'nombre': 'Boogerman', 'plataforma': 'SEGA genesis'},
+        {'nombre_usuario': 'Josefina Botto', 'fecha': '2/02/26', 'nombre': 'Cadillacs & Dinosaurs', 'plataforma': 'ARCADE'},
+        {'nombre_usuario': 'Jorge Gonzalez', 'fecha': '23/02/26', 'nombre': 'Bloody Roar', 'plataforma': 'PlayStation'}
     ]
 
     contexto_post_juego = {'post_juego': juegos}
-    return render(request, 'blog/post_juego.html', contexto_post_juego)
+    return render(request, 'blog/blog.html', contexto_post_juego)
