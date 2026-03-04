c58b407516bc23552e1340f26ba5d9ba6f2f7d92
Gabriel Gonzalez <gabrielgonzalezbotto@gmail.com>
2026-03-04 15:39:29 -0300

    arreglando cards de blog y responsividad

--- blog_videojuegos/blog/views.py
+++ blog_videojuegos/blog/views.py
@@ -6,7 +6,9 @@ def post_juego(request):
         {'nombre_usuario': 'Gabriel Gonzalez Botto', 'fecha': '22/02/26', 'nombre': 'Battletoads', 'plataforma':'SEGA genesis'},
         {'nombre_usuario': 'Martina Gonzalez', 'fecha': '15/02/26', 'nombre': 'Boogerman', 'plataforma': 'SEGA genesis'},
         {'nombre_usuario': 'Josefina Botto', 'fecha': '2/02/26', 'nombre': 'Cadillacs & Dinosaurs', 'plataforma': 'ARCADE'},
-        {'nombre_usuario': 'Jorge Gonzalez', 'fecha': '23/02/26', 'nombre': 'Bloody Roar', 'plataforma': 'PlayStation'}
+        {'nombre_usuario': 'Jorge Gonzalez', 'fecha': '23/02/26', 'nombre': 'Bloody Roar', 'plataforma': 'PlayStation'},
+        {'nombre_usuario': 'Jorge Gonzalez', 'fecha': '23/02/26', 'nombre': 'Bloody Roar', 'plataforma': 'PlayStation'},
+        {'nombre_usuario': 'Josefina Botto', 'fecha': '2/02/26', 'nombre': 'Cadillacs & Dinosaurs', 'plataforma': 'ARCADE'},
     ]
 
     contexto_post_juego = {'post_juego': juegos}
