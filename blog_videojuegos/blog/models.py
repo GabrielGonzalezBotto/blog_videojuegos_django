ada2ef77404f69dec195c35e9de5362441253e74
Gabriel Gonzalez <gabrielgonzalezbotto@gmail.com>
2026-03-12 15:10:48 -0300

    creacion modelo y vista de juegos e migracion a base de datos

--- blog_videojuegos/blog/models.py
+++ blog_videojuegos/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Juego(models.Model):
+    user = models.CharField(max_length=100)
+    fecha = models.DateField(max_length=8)
+    nombre = models.CharField(max_length=100)
+    plataforma = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nombre
--- blog_videojuegos/blog/views.py
+++ blog_videojuegos/blog/views.py
@@ -1,15 +1,8 @@
 from django.shortcuts import render
+from blog.models import Juego
 
 # Create your views here.
 def post_juego(request):
-    juegos = [
-        {'nombre_usuario': 'Gabriel Gonzalez Botto', 'fecha': '22/02/26', 'nombre': 'Battletoads', 'plataforma':'SEGA genesis'},
-        {'nombre_usuario': 'Martina Gonzalez', 'fecha': '15/02/26', 'nombre': 'Boogerman', 'plataforma': 'SEGA genesis'},
-        {'nombre_usuario': 'Josefina Botto', 'fecha': '2/02/26', 'nombre': 'Cadillacs & Dinosaurs', 'plataforma': 'ARCADE'},
-        {'nombre_usuario': 'Jorge Gonzalez', 'fecha': '23/02/26', 'nombre': 'Bloody Roar', 'plataforma': 'PlayStation'},
-        {'nombre_usuario': 'Jorge Gonzalez', 'fecha': '23/02/26', 'nombre': 'Bloody Roar', 'plataforma': 'PlayStation'},
-        {'nombre_usuario': 'Josefina Botto', 'fecha': '2/02/26', 'nombre': 'Cadillacs & Dinosaurs', 'plataforma': 'ARCADE'},
-    ]
-
+    juegos = Juego.objects.all()
     contexto_post_juego = {'post_juego': juegos}
     return render(request, 'blog/blog.html', contexto_post_juego)
