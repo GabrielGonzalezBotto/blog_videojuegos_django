9f18048e93e89204c577e5a76b2e6f549b1a9690
Gabriel Gonzalez <gabrielgonzalezbotto@gmail.com>
2026-03-23 16:39:12 -0300

    nueva paginación

--- blog_videojuegos/blog/views.py
+++ blog_videojuegos/blog/views.py
@@ -3,16 +3,18 @@ from django.core.paginator import Paginator
 from blog.models import Juego
 
 # Create your views here.
-def post_juego(request):
-    juegos = Juego.objects.all()
-    contexto_post_juego = {'post_juego': juegos}
-    return render(request, 'blog/blog.html', contexto_post_juego)
 
-def lista_post(request):
-    juegos = Juego.objects.all().order_by('id') #Mantenemos un orden consistente
-    paginator = Paginator(Juego, 1) #Mostramos juegos por pagina
-    page_number = request.GET.get('page') #Obtenemos el numero de pagina desde la URL (?page=2)
-    page_obj = paginator.get_page(page_number) #Obtenemos los objetos de esa pagina
-    #Pasamos a plantilla como 'lista_post'
-    contexto_post_juego = {'post_juego': page_obj}
-    return render(request, 'blog/blog.html', contexto_post_juego)
+
+#def post_juego(request):
+    #juegos = Juego.objects.all()
+    #contexto_post_juego = {'post_juego': juegos}
+    #return render(request, 'blog/blog.html', contexto_post_juego)
+
+#def lista_post(request):
+    #juegos = Juego.objects.all().order_by('id') #Mantenemos un orden consistente
+    #paginator = Paginator(Juego, 1) #Mostramos juegos por pagina
+    #page_number = request.GET.get('page') #Obtenemos el numero de pagina desde la URL (?page=2)
+    #page_obj = paginator.get_page(page_number) #Obtenemos los objetos de esa pagina
+     #Pasamos a plantilla como 'lista_post'
+    #contexto_post_juego = {'post_juego': page_obj}
+    #return render(request, 'blog/blog.html', contexto_post_juego)
