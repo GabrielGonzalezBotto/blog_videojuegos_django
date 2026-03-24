8c95f489750ead6287cb7484e114079434f0e833
Gabriel Gonzalez <gabrielgonzalezbotto@gmail.com>
2026-03-24 15:37:21 -0300

    nuevos cambios de paginacion

--- blog_videojuegos/blog/urls.py
+++ blog_videojuegos/blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from  . import views
 
 urlpatterns = [
-    path('', views.post_juego, name='blog')
+    path('', views.post_juego, name='blog'),
+    path('', views.lista_juegos, name='blog')
 ]
--- blog_videojuegos/blog/views.py
+++ blog_videojuegos/blog/views.py
@@ -4,6 +4,24 @@ from blog.models import Juego
 
 # Create your views here.
 
+def post_juego(request):
+    juegos = Juego.objects.all()
+    contexto_post_juego = {'post_juego': juegos}
+    return render(request, 'blog/blog.html', contexto_post_juego)
+    
+
+
+
+def lista_juegos(request):
+    juegos = Juego.objects.all().order_by('id') #Mantenemos un orden consistente
+    paginator = Paginator(juegos, 1) #Mostramos 8 juegos por paginas
+    #Obtenbemos el numero de paginas desde la URL (?page=2)
+    page_number = request.GET.get('page')
+    #Obetnemos los objetos de esa pagina
+    page_obj = paginator.get_page(page_number)
+    #Pasamos a la plantilla como lista_juegos
+    contexto_blog_juegos = {'lista_juegos': page_obj}
+    return render(request, 'blog/blog.html', contexto_blog_juegos)
 
 #def post_juego(request):
     #juegos = Juego.objects.all()
