199d357b68cdb8a2b13d5bb4b9e0a06cdb1ea47d
Gabriel Gonzalez <gabrielgonzalezbotto@gmail.com>
2026-04-24 10:57:38 -0300

    importando views

--- blog_videojuegos/usuarios/views.py
+++ blog_videojuegos/usuarios/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+from  forms import RegistroForm, loginForm
+from django.contrib import messages
 
 # Create your views here.
+
+
