0521cc01034a1874f370ed8e193bc881ec10105a
Gabriel Gonzalez <gabrielgonzalezbotto@gmail.com>
2026-04-30 15:30:35 -0300

    modificaciones en settings, forms y agregado de plantillas

--- blog_videojuegos/blog_videojuegos/settings.py
+++ blog_videojuegos/blog_videojuegos/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS=[BASE_DIR / 'static']
 
-#AUTH_USER_MODEL = 'usuarios.Usuario'
-#LOGIN_URL = 'login'
-#LOGIN_REDIRECT_URL = 'home'
-#LOGOUT_REDIRECT_URL = 'login'
+AUTH_USER_MODEL = 'usuarios.Usuario'
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'login'
--- blog_videojuegos/usuarios/forms.py
+++ blog_videojuegos/usuarios/forms.py
@@ -1,3 +1,12 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import Usuario
+
+class RegistroForms(UserCreationForm):
+    class Meta:
+        model = Usuario
+        fileds = ['username', 'email', 'password1', 'password2', ]
+
+class LoginForm(AuthenticationForm):
+    # No hace falta Meta
+    pass
