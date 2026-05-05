351040a1a9817eab68296c59e2ec668fa7c35148
Gabriel Gonzalez <gabrielgonzalezbotto@gmail.com>
2026-05-05 15:15:14 -0300

    configuración de admin.py

--- blog_videojuegos/usuarios/admin.py
+++ blog_videojuegos/usuarios/admin.py
@@ -2,3 +2,26 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import Usuario
 # Register your models here.
+
+class UsuarioAdmin(UserAdmin):
+    #Campos que se mostrarán en la lista de usuarios
+    list_display = ('username', 'email')
+    list_filter = ('is_staff', 'is_superuser', 'is_active')
+
+    #Campos que se mostrarán al editar un usuario
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        ('Permisos', {'fields':('is_staff', 'is_superuser', 'is_active', 'groups', 'user_permissions')}),
+        ('Fechas importantes', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password', 'password2', 'is_staff', 'is_active')
+        }),
+    )
+    search_fields = ('username', 'email')
+    ordering = ('username')
+
+admin.site.register(Usuario, UsuarioAdmin)
