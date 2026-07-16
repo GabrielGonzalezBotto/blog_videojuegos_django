a1e1ed74ae5f97bfd2f54e5df29255d33583ecc9
Gabriel Gonzalez <gabrielgonzalezbotto@gmail.com>
2026-07-16 16:29:37 -0300

    add: seccion populares/recientes, views.py

--- blog_videojuegos/inicio/views.py
+++ blog_videojuegos/inicio/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from django.db.models import Count
+from blog.models import Juego
 
 # Create your views here.
 def index(request):
+    juegos_populares = Juego.objects.annotate(q_likes=Count('likes')).order_by('-q_likes')[:3]
+    juegos_recientes = Juego.objects.all().order_by('-id')[:3]
+    contexto = {
+        'juegos_populares': juegos_populares,
+        'juegos_receintes': juegos_recientes
+    }
     return render(request, 'inicio/index.html')
 
 def contacto(request):
