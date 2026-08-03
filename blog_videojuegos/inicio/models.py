5deebf6c0654e85aae4ae07cd90550e3982649e7
Gabriel Gonzalez <gabrielgonzalezbotto@gmail.com>
2026-08-03 15:40:50 -0300

    add: model Contatcto y migraciones

--- blog_videojuegos/inicio/models.py
+++ blog_videojuegos/inicio/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Contacto(models.Model):
+    nombre = models.CharField(max_length=100)
+    email = models.EmailField()
+    mensaje = models.TextField()
+    decha = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Mensaje de {self.nombre} - {self.email}"
